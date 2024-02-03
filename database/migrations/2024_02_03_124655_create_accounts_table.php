3e98332e65da2e67e68080e3d176fe0b7fab5718
Rafael Andrade <rafael.io@outlook.com>
2024-02-03 12:43:44 -0300

    Implementação Migrations

---
+++ database/migrations/2024_02_03_124655_create_accounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id('index');
+            $table->uuid('id');
+            $table->uuid('client_id');
+            $table->decimal('balance', 10, 2);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
