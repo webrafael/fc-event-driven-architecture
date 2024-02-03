53ce0c36044ed4b7b61ba79fe52926e3ee66f446
Rafael Andrade <rafael.io@outlook.com>
2024-02-03 16:09:05 -0300

    Correção KafkaProducer

--- wallet/Integration/Kafka/KafkaProducer.php
+++ wallet/Integration/Kafka/KafkaProducer.php
@@ -1,22 +1,43 @@
 <?php namespace Wallet\Integration\Kafka;
 
+use RdKafka\Conf;
 use RdKafka\Producer;
+use Wallet\Integration\Events\Exception\InvalidEventException;
 
 class KafkaProducer
 {
+    protected $port = 29092;
+    protected $host = 'kafka';
+
     protected $producer;
 
     public function __construct()
     {
-        $this->producer = new Producer();
+        $conf = new Conf;
+        $conf->set('debug', 'all');
+        $conf->set('metadata.broker.list', "{$this->host}:{$this->port}");
+
+        $conf->setErrorCb(function (Producer $producer, int $errorCode, string $errorMsg) {
+            throw new \RuntimeException($errorMsg, $errorCode);
+        });
+
+        $this->producer = new Producer($conf);
+        $this->producer->addBrokers("{$this->host}:{$this->port}");
         $this->producer->setLogLevel(LOG_DEBUG);
-        $this->producer->addBrokers("kafka:9092");
+        $this->producer->setLogger(RD_KAFKA_LOG_SYSLOG);
     }
 
     public function produce($topicName, $message)
     {
-        $topic = $this->producer->newTopic($topicName);
-        $topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
-        $this->producer->poll(0);
+        try {
+            $topic = $this->producer->newTopic($topicName);
+            $topic->produce(RD_KAFKA_PARTITION_UA, 0, $message);
+
+            while ($this->producer->getOutQLen() > 0) {
+                $this->producer->flush(1000);
+            }
+        } catch(\Exception $exc) {
+            throw new InvalidEventException($exc->getMessage(), 500);
+        }
     }
 }
