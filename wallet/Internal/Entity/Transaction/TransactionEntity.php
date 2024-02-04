584e8d95f5c9ff9cf5691cf715e648cc07d5615a
Rafael Andrade <rafael.io@outlook.com>
2024-02-04 11:29:18 -0300

    Melhorias de código

--- wallet/Internal/Entity/Transaction/TransactionEntity.php
+++ wallet/Internal/Entity/Transaction/TransactionEntity.php
@@ -2,7 +2,6 @@
 
 use DateTime;
 use Wallet\Internal\Entity\Account\AccountEntity;
-use Wallet\Internal\Entity\Exception\InvalidEntityException;
 
 class TransactionEntity
 {
@@ -12,37 +11,24 @@ class TransactionEntity
         public ?string $accountFromId = null,
         public ?AccountEntity $accountTo = null,
         public ?string $accountToId = null,
-        public float $amount = 0.0,
+        public float|int $amount = 0.0,
         public ?DateTime $createdAt = null,
     ) {
-        $this->validation();
-
         $this->debitFromAccount();
         $this->creditToAccount();
     }
 
-    public function creditToAccount(): float
+    public function creditToAccount(): void
     {
-        $this->accountTo->credit($this->amount);
-
-        return $this->accountTo->balance;
-    }
-
-    public function debitFromAccount(): float
-    {
-        $this->accountFrom->debit($this->amount);
-
-        return $this->accountFrom->balance;
-    }
-
-    public function validation()
-    {
-        if ($this->amount <= 0.0) {
-            throw new InvalidEntityException("O valor deve ser maior que zero");
+        if (! is_null($this->accountTo)) {
+            $this->accountTo->credit($this->amount);
         }
+    }
 
-        if ($this->accountFrom->balance < $this->amount) {
-            throw new InvalidEntityException("Saldo insuficiente");
+    public function debitFromAccount(): void
+    {
+        if (! is_null($this->accountFrom)) {
+            $this->accountFrom->debit($this->amount);
         }
     }
 }
